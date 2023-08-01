9073e56b619f4046331c163ab7aa9e0cdb855d5b
singhVijender05 <kvsingh709@gmail.com>
2023-08-01 10:40:45 +0530

    added split function to split pdf using page limit

--- pdf_merger.py
+++ pdf_merger.py
@@ -27,3 +27,27 @@ def pdf_merger():
     except:
         print('error opening pdfs either pdf does\'t exists or been corrupted')
 
+
+#function to slice pdf using page limit(partition factor)
+#for example if total page in original pdf are 25 and you select partition factor as 5 then 5 pdfs of 5 pages each will be generated
+def splitpdf(input_file,page_limit):
+    #configure pdf reader
+    pdf_reader=PyPDF2.PdfReader(input_file)
+    #now this pdfReader can access all the content or you can say pages
+    total_pages=len(pdf_reader.pages)
+    
+    for start in range(0,total_pages,page_limit):
+        end=start+page_limit
+        if(total_pages<end):
+            end=total_pages
+        output_file=f'output_{start}-{end}.pdf' #name output file
+        pdf_writer=PyPDF2.PdfWriter()
+        for i_page in range(start,end):
+            page=pdf_reader.pages[i_page]
+            pdf_writer.add_page(page) #add page to writer
+        with open(output_file,"wb") as outputFile:
+            # now writer will write all the pages to output file
+            pdf_writer.write(outputFile)
+        print(f'created output file as {output_file}')
+
+
